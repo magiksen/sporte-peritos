1fc2f99f62910e053bad6a10db0726e9db7762d2
magiksen <elemejore@gmail.com>
2022-07-26 16:31:32 -0400

    Configuracion correcta del config database

--- config/database.php
+++ config/database.php
@@ -81,8 +81,8 @@ return [
         'pgsql2' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
+            'host' => env('DB_HOST_SECOND', '127.0.0.1'),
+            'port' => env('DB_PORT_SECOND', '5432'),
             'database' => env('DB_DATABASE_SECOND', 'forge'),
             'username' => env('DB_USERNAME_SECOND', 'forge'),
             'password' => env('DB_PASSWORD_SECOND', ''),
